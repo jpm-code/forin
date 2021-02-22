b6cee1337a664437e2670890e25ecb4318ef8376
jpm-code <you@example.com>
2021-02-22 08:34:14 -0500

    lab setup

---
+++ lab3.sh
@@ -0,0 +1,6 @@
+sudo apt install vim git ssh
+sudo apt install python3-pip python3-virtualenv libssl-dev
+sudo apt install libffi-dev build-essential libpython3-dev 
+sudo apt install python3-minimal authbind
+sudo apt install virtualenv nano
+
